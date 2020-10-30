f7e33bcca8da089786e048016105cf5b6d8d3068
dominatordq <dominatordq@gmail.com>
2020-10-30 13:57:55 -0600

    Trying to explicitly define the database from Atlas

--- startup/db.js
+++ startup/db.js
@@ -4,7 +4,8 @@ const config = require('config');
 
 // Handles all of the database startup/management
 module.exports = function() {
-    const db = config.get('db')
+    // const db = config.get('db');
+    const db = 'mongodb+srv://vidlyuser:1234@vidly.6jxtf.mongodb.net/sandbox?retryWrites=true&w=majority';
     mongoose.connect(db, { useNewUrlParser: true })
         .then(() => winston.info(`Connected to ${db}...`));
 }
