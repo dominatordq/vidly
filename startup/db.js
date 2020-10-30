09efac083fc7f7dae8a3a76e8995fc6767735249
dominatordq <dominatordq@gmail.com>
2020-10-30 14:00:42 -0600

    Going back to original way to get database name

--- startup/db.js
+++ startup/db.js
@@ -4,8 +4,7 @@ const config = require('config');
 
 // Handles all of the database startup/management
 module.exports = function() {
-    // const db = config.get('db');
-    const db = 'mongodb+srv://vidlyuser:1234@vidly.6jxtf.mongodb.net/sandbox?retryWrites=true&w=majority';
+    const db = config.get('db');
     mongoose.connect(db, { useNewUrlParser: true })
         .then(() => winston.info(`Connected to ${db}...`));
 }
